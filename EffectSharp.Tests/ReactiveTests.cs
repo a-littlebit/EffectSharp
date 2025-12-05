685279f609fecda7e7b378ca5f5e77052be4f37e
vvcoder <mzp.2005@qq.com>
2025-12-05 17:28:59 +0800

    test: check property change event sender.

--- EffectSharp.Tests/ReactiveTests.cs
+++ EffectSharp.Tests/ReactiveTests.cs
@@ -5,26 +5,29 @@ namespace EffectSharp.Tests
     public class ReactiveTests
     {
         [Fact]
-        public async Task Reactive_WhenSetProperty_TriggerPropertyChange()
+        public async Task Reactive_WhenSetProperty_NotifyPropertyChange()
         {
             var product = Reactive.Create(new Product
             {
                 Name = "Laptop",
                 Price = 1000
             });
+            object? notifier = null;
             string? changedPropertyName = null;
             ((INotifyPropertyChanged)product)!.PropertyChanged += (sender, e) =>
             {
+                notifier = sender;
                 changedPropertyName = e.PropertyName;
             };
             product.Price = 1200;
             // Wait for the next tick to ensure the PropertyChanged event is raised
             await TaskManager.FlushNotifyQueue();
+            Assert.Same(product, notifier);
             Assert.Equal(nameof(product.Price), changedPropertyName);
         }
 
         [Fact]
-        public async Task Reactive_WhenSetNestedProperty_TriggerPropertyChange()
+        public async Task Reactive_WhenSetNestedProperty_NotifyPropertyChange()
         {
             var order = Reactive.CreateDeep(new Order
             {
@@ -35,19 +38,22 @@ namespace EffectSharp.Tests
                 },
                 Quantity = 2
             });
+            object? notifier = null;
             string? changedPropertyName = null;
             ((INotifyPropertyChanged)order.Product)!.PropertyChanged += (sender, e) =>
             {
+                notifier = sender;
                 changedPropertyName = e.PropertyName;
             };
             order.Product.Price = 600;
             // Flush the notify queue to ensure the PropertyChanged event is raised
             await TaskManager.FlushNotifyQueue();
+            Assert.Same(order.Product, notifier);
             Assert.Equal(nameof(order.Product.Price), changedPropertyName);
         }
 
         [Fact]
-        public async Task Reactive_WhenSetProperty_NotifySubcribers()
+        public async Task Reactive_WhenSetProperty_TriggerSubcribers()
         {
             var product = Reactive.Create(new Product
             {
