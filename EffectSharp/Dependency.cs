6c10790db19dd6c8642b21e662744a565e159283
vvcoder <mzp.2005@qq.com>
2025-12-31 17:29:14 +0800

    perf(Dependency): lazily create subcriber dictionary.

--- EffectSharp/Dependency.cs
+++ EffectSharp/Dependency.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EffectSharp
@@ -12,7 +13,19 @@ namespace EffectSharp
     /// </summary>
     public class Dependency
     {
-        private readonly ConcurrentDictionary<Effect, bool> _subscribers = new();
+        private ConcurrentDictionary<Effect, bool>? _subscribers;
+
+        private ConcurrentDictionary<Effect, bool> Subscribers
+        {
+            get
+            {
+                var s = _subscribers;
+                if (s != null) return s;
+
+                var created = new ConcurrentDictionary<Effect, bool>();
+                return Interlocked.CompareExchange(ref _subscribers, created, null) ?? created;
+            }
+        }
 
         /// <summary>
         /// Adds an effect as a subscriber to this dependency.
@@ -21,7 +34,7 @@ namespace EffectSharp
         /// <returns>True if added; false if already present.</returns>
         public bool AddSubscriber(Effect effect)
         {
-            return _subscribers.TryAdd(effect, true);
+            return Subscribers.TryAdd(effect, true);
         }
 
         /// <summary>
@@ -31,7 +44,7 @@ namespace EffectSharp
         /// <returns>True if removed; false if not found.</returns>
         public bool RemoveSubscriber(Effect effect)
         {
-            return _subscribers.TryRemove(effect, out _);
+            return Subscribers.TryRemove(effect, out _);
         }
 
         /// <summary>
@@ -56,7 +69,7 @@ namespace EffectSharp
         /// </summary>
         public void Trigger()
         {
-            foreach (var subscriber in _subscribers.Keys)
+            foreach (var subscriber in Subscribers.Keys)
             {
                 subscriber.ScheduleExecution();
             }
